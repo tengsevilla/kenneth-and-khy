80405e5d18032d5cbaa3414f704d06bf40471cbe
Anthony Sevilla <SevillAnthonyy@gmail.com>
2024-11-30 11:14:38 +0800

    Added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,28 @@ import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
-  title: "K & K Wedding Invite",
-  description: "Kenneth & Ky Wedding Invitaiton",
+  title: "Kenneth & Khy Wedding",
+  description: "Join us to celebrate our union on December 22, 2024, at Nature's Village Resort.",
   icons: {
     icon: [
       { rel: "icon", url: "/icon.png" },
     ]
   },
+  openGraph: {
+    title: "Kenneth & Khy Wedding",
+    description: "Join us to celebrate our union on December 22, 2024, at Nature's Village Resort.",
+    url: "https://kenneth-and-khy.vercel.app",
+    images: [
+      {
+        url: "/preview.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Kenneth & Khy Wedding",
+      },
+    ],
+    type: "website",
+  },
+  metadataBase: new URL("https://kenneth-and-khy.vercel.app"),
 };
 
 export const libreFranklin = Libre_Franklin({
